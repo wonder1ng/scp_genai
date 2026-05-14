bb093ec23fb064e3851135e3856b6c2aabb3e5cb
wonder1ng <wonder1ng@velog.io>
2026-05-14 16:57:27 +0900

    practicescrapy

---
+++ 007_PYTHON/scrap.py
@@ -0,0 +1,57 @@
+import requests, csv, os
+basePath = "\\".join(__file__.split("\\")[:-1])
+with open(basePath + "\\env.txt", "r", encoding="utf_8") as f:
+    res = [line.split("=")[1] for line in f.read().split("\n")]
+    baseUrl = res[0]
+    id = res[1]
+    pw = res[2]
+
+data = []
+for i in (1, 2):
+    res = requests.get(f"{baseUrl}/api/products?page={i}")
+    data += res.json()["items"]
+for p in data:
+    res = requests.get(f"{baseUrl}/api/product/{p["product_id"]}")
+    detail = res.json()
+    p["sales_count"] = detail.get("sales_count")
+    p["reviews"] = detail.get("reviews")
+
+basePath = "\\".join(__file__.split("\\")[:-1])
+fileName = basePath + "\\logout.tsv"
+with open(fileName, "w", newline="", encoding="utf_8") as f:
+    csv_writer = csv.DictWriter(f, data[0].keys(), delimiter="\t")
+    csv_writer.writeheader()
+    csv_writer.writerows(data)
+with open(fileName, "r", encoding="utf_8") as f:
+    csv_reader = csv.DictReader(f)
+    [print(r) for r in csv_reader]
+
+from playwright.sync_api import sync_playwright
+
+data = []
+
+with sync_playwright() as p:
+    browser = p.chromium.launch()
+    page = browser.new_page()
+    page.goto(baseUrl)
+    page.locator("#uid").fill(id)
+    page.locator("#upw").fill(pw)
+    page.locator("#loginBtn").click()
+    for i in range(1, 6):
+        res = page.request.get(f"{baseUrl}/api/products?page={i}")
+        data += res.json()["items"]
+    for p in data:
+        res = page.request.get(f"{baseUrl}/api/product/{p["product_id"]}")
+        detail = res.json()
+        p["sales_count"] = detail.get("sales_count")
+        p["reviews"] = detail.get("reviews")
+
+basePath = "\\".join(__file__.split("\\")[:-1])
+fileName = basePath + "\\login.tsv"
+with open(fileName, "w", newline="", encoding="utf_8") as f:
+    csv_writer = csv.DictWriter(f, data[0].keys(), delimiter="\t")
+    csv_writer.writeheader()
+    csv_writer.writerows(data)
+with open(fileName, "r", encoding="utf_8") as f:
+    csv_reader = csv.DictReader(f)
+    [print(r) for r in csv_reader]
